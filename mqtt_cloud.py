cfc544484d7e9ec2a237536910a057bc1e72b3f4
OBriqa <omar.briqa@estudiantat.upc.edu>
2023-06-27 18:38:01 +0200

    use api env var

--- mqtt_cloud.py
+++ mqtt_cloud.py
@@ -2,6 +2,9 @@ import paho.mqtt.client as mqtt
 import json
 import requests
 import logging
+import os
+
+api_url = os.environ.get('API')
 
 def is_json(data):
     try:
@@ -105,7 +108,7 @@ def on_message(client, userdata, msg):
             #YA EXSITE: license_plate,capacitylast,_maintenance_date,beehive,location_in,location_act,location_end
             if all(key in payload for key in needed_keys):                
                 logging.info("update llega aqui siu")
-                url = 'http://api:5000/api/update_location'
+                url = api_url + "/api/update_location"
                 response =  requests.post(url, json = payload)
             else: 
                 logging.info("SUBNORMAL!!! ENVIA BIEN LOS CAMPOS | UPDATELOCATION")
@@ -119,7 +122,7 @@ def on_message(client, userdata, msg):
             #si el mensaje es que que queremos, entonces lo tratamos
             if all(key in payload for key in needed_keys):                
                 logging.info("status llega aqui siu")
-                url = 'http://api:5000/api/update_status'
+                url = api_url + "/api/update_status"
                 response =  requests.post(url, json = payload)
             else: 
                 logging.info("SUBNORMAL!!! ENVIA BIEN LOS CAMPOS | UPDATESTATUS")          
--- mqtt_edge.py
+++ mqtt_edge.py
@@ -11,12 +11,7 @@ import os
 mongo_host = os.environ.get('DB_HOST')
 mongo_port = os.environ.get('DB_PORT')
 mqtt_topic_city = os.environ.get('MQTT_TOPIC_CITY')
-
-client = MongoClient('mongodb://root:root@'+mongo_host+':'+mongo_port)
-db = client['PTIN']
-dron = db['Drones']
-orders = db['Orders']
-
+api_url = os.environ.get('API')
 
 def is_json(data):
     try:
@@ -30,132 +25,27 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("PTIN2023/#")
 
 def on_message(client, userdata, msg):
-    #Aqui podemos poner los ifs para definir un tema determinado
     
     #----------------------------------------------------------------------------------------
     if msg.topic == "PTIN2023/"+mqtt_topic_city+"/DRON/UPDATELOCATION":
         if(is_json(msg.payload.decode('utf-8'))):
-            #tratamiento del mensaje-->
             payload = json.loads(msg.payload.decode('utf-8'))
-            needed_keys = ['id_dron','location_act','status','status_num','battery','autonomy']
-            #si el mensaje es que que queremos, entonces lo tratamos
+            needed_keys = ['id_dron', 'location_act', 'status', 'status_num', 'battery', 'autonomy']
             if all(key in payload for key in needed_keys):                
-                print("---------------------------------------")
-                update_fields = {
-                    'location_act': payload['location_act'],
-                    'status': payload['status'],
-                    'battery': payload['battery'],
-                    'autonomy': payload['autonomy'],
-                    'status_num' : payload['status_num']
-                }
-                # Filtra por id_car y actualiza los demás campos
-                try:
-                    result = dron.update_one(
-                        {'id_dron': payload['id_dron']},  # Filtra por el valor de id_car
-                        {'$set': update_fields}  # Define los campos y valores a actualizar
-                    )
-                    if result.modified_count > 0:
-                        print("Documento actualizado correctamente")
-                    else:
-                        print(str(payload['id_dron']))
-                        print("El documento no se actualizó. Puede que no se encontrara el id_dron especificado.UPDATELOCATION")
-                        
-                except PyMongoError as e:
-                    print("Ocurrió un error al actualizar el documento:", str(e))
-                    
+                logging.info("update llega aqui siu")
+                url = api_url + "/api/update_location"
+                response =  requests.post(url, json = payload)
             else: 
-                print("SUBNORMAL!!! ENVIA BIEN LOS CAMPOS | UPDATELOCATION")
+                logging.info("SUBNORMAL!!! ENVIA BIEN LOS CAMPOS | UPDATELOCATION")
+                
 #-----------------------------------------------------------------------------------------    
     if msg.topic == "PTIN2023/"+mqtt_topic_city+"/DRON/UPDATESTATUS":#Hay que poner un tema en común, sinó se petará todo
        
-
         if(is_json(msg.payload.decode('utf-8'))):
-            #tratamiento del mensaje-->
             payload = json.loads(msg.payload.decode('utf-8'))
-            needed_keys = ['id_dron', 'status','status_num']
-            #si el mensaje es que que queremos, entonces lo tratamos
-            if all(key in payload for key in needed_keys):                
-                # print("---------------------------------------")
-                # print('Estado: ', payload['status_num'])
-                # if payload['status_num'] == 3 or payload['status_num']==4 or payload['status_num']==9 or payload['status_num']==10:
-                #     print("entro")
-                #     if payload['status_num']==3:
-                #         state='drone_sent'
-                #         state_num=4
-                #     elif payload['status_num']==4:
-                #         state='delivered_awaiting'
-                #         state_num=5
-                #     elif payload['status_num']==9:
-                #         state='delivered'
-                #         state_num=9
-                #     elif payload['status_num']==10:
-                #         state='not delivered'
-                #         state_num=10
+            needed_keys = ['id_dron', 'status', 'status_num']
+            if all(key in payload for key in needed_keys):
+                logging.info("status llega aqui siu")
+                url = api_url + "/api/update_status"
+                response =  requests.post(url, json = payload)              
                     
-                #     print("status: " , payload['status_num'])
-
-                #     id_pack=dron.find_one(
-                #         {'id_dron': payload['id_dron']}, 
-                #         {'id_pack': 1}
-                #     )['id_pack']
-                    
-                #     print("idpacl= "+str(id_pack))
-                #     update_fields2 = {
-                #         'state': state,
-                #         'state_num' : state_num
-                #     }
-                #     Response=orders.update_one(
-                #     {'order_identifier': id_pack},  # Filtra por el valor de id_pack
-                #     {'$set': update_fields2}  # Define los campos y valores a actualizar
-
-                #     )                
-                
-                
-                
-                
-                
-                
-                logging.info(mqtt_topic_city)
-                update_fields = {
-                    'status': payload['status'],
-                    'status_num' : payload['status_num']
-                }
-                # Filtra por id_car y actualiza los demás campos
-                try:
-                    result = dron.update_one(
-                        {'id_dron': payload['id_dron']},  # Filtra por el valor de id_car
-                        {'$set': update_fields}  # Define los campos y valores a actualizar
-                    )
-                    if result.modified_count > 0:
-                        print("Documento actualizado correctamente")
-                        logging.info("Documento actualizado correctamente")
-                    else:
-                        print(str(payload['id_dron']))
-                        logging.info("El documento no se actualizó. Puede que no se encontrara el id_dron especificado.updatestatus")
-                        print("El documento no se actualizó. Puede que no se encontrara el id_dron especificado.updatestatus")
-                        
-                except PyMongoError as e:
-                    print("Ocurrió un error al actualizar el documento:", str(e))
-                    logging.info("Ocurrió un error al actualizar el documento:", str(e))
-            else: 
-                print("SUBNORMAL!!! ENVIA BIEN LOS CAMPOS | UPDATESTATUS")      
-                logging.info("SUBNORMAL!!! ENVIA BIEN LOS CAMPOS | UPDATESTATUS")    
-#-----------------------------------------------------------------------------------------    
-
-
-
-    if msg.topic == "PTIN2023/"+mqtt_topic_city+"/A2/FRAN/JOA":#Hay que poner un tema en común, sinó se petará todo
-        if(is_json(msg.payload.decode('utf-8'))):
-            #tratamiento del mensaje-->
-            payload = json.loads(msg.payload.decode('utf-8'))
-            needed_keys = ['id_dron', 'status']
-            #si el mensaje es que que queremos, entonces lo tratamos
-            if all(key in payload for key in needed_keys):                
-                
-                print("Enviado a la BD id_dron " + str(payload["id_dron"]) +" y status" + str(payload["status"]))
-                print("---------------------------------------")
-            
-            else:
-                print("PTIN2023/A2/FRAN")        
-        else:
-            print("Recibido el texto plano siguiente: "+msg.payload.decode('utf-8'))
